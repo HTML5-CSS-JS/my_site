6cd0dc35dc430d7d82b77b7f9df7620571e3a85f
asdf <sumiminwooo@gmail.com>
2026-02-11 17:08:33 +0900

    Fix style.css endpoint and remove duplicate code

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+@app.get("/style.css")
+    css = sass.compile(filename="/style.scss")
+    return Response(content=css, media_type="text/css")
 # static 디렉토리 연결 (예: style.css, script.js 등)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=FileResponse)
 def home():
     return "index.html"
-@app.get("/style.css")
-    css = sass.compile(filename="/style.scss")
-    return Response(content=css, media_type="text/css")

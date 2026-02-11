e5998b598494621e3900cee7eb65173d8a28251b
asdf <sumiminwooo@gmail.com>
2026-02-11 16:58:15 +0900

    Add home endpoint to log a message
    Added a home endpoint that logs a message.

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ app = FastAPI()
 
 @app.get("/")
 @app.get("/style.css")
+def home():
+    return print(t"log")
 def get_css():
     css = sass.compile(filename="/style.scss")
     return Response(content=css, media_type="text/css")

8d12c7c7f1f0e60008d93e9eff328541afa3bfed
asdf <sumiminwooo@gmail.com>
2026-02-12 11:49:01 +0900

    Replace sass with sass_embedded for CSS compilation

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import sass
 from fastapi import FastAPI, Response
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from sass_embedded import compile_file
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=FileResponse)
-# 정적 파일 연결 (예: 이미지, JS 등)
-@app.mount("/static", StaticFiles(directory="static"), name="static")
 def home():
     return FileResponse("index.html")
+
 @app.get("/style.css")
 def style():
-    css = sass.compile(filename="style.scss")
+    css = compile_file("style.scss")
     return Response(content=css, media_type="text/css")

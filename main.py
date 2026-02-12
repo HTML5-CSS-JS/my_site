88403f181cf5a07e729401f602062dc9d246653a
asdf <sumiminwooo@gmail.com>
2026-02-12 10:51:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,12 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# SCSS → CSS 컴파일 후 반환
+@app.get("/", response_class=FileResponse)
+# 정적 파일 연결 (예: 이미지, JS 등)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+def home():
+    return FileResponse("index.html")
 @app.get("/style.css")
 def style():
     css = sass.compile(filename="style.scss")
     return Response(content=css, media_type="text/css")
-    
-@app.get("/", response_class=FileResponse)
-# 정적 파일 연결 (예: 이미지, JS 등)
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-def home():
-    return FileResponse("index.html")
